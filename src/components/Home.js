a1f539a1365b0de92752e17570e16fb85ce11102
nadelacruz <nicoangelodelacruz@gmail.com>
2021-06-13 16:18:27 +0800

    Changed server for homepage image.

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
           <Image
             htmlWidth="500px"
             borderRadius="full"
-            src="https://lh3.googleusercontent.com/8_kURm8HZtYW2FG_JPqWE3ZrDTbi3uX8SX_-wlgkV5YNhUUmDZXeuIzIwJ1Why0r_OhMtMc733kHTwBnxlG4cgBKjbpYzw-lt_6LDVg6Voklw4GAXpm7oEntL3_crdPiq1uJ-5dnmHhueZ4XzL5zZFLP1O6O-LUvP7DnzjWGMJWCkm2F-bBWAaOn8I9W30Nsad2BereQ_iOypqI7u27mrygzzowqv9woVN5os81khpsSdvFCS1Jvwu5OoriZFB32nW2kPw2FO-0NB_f83u-CTiepxI9Q8P1tTbVxB3tSgRPFf0RktI0i9EF0-rTl-yhfanKbqULl0_-yCNCaMe6Sh41aBidyZLiZzt5iMrEpSqDLAfDK0kNLdpxCg26ePapb1m_T48PtIPmBxWGQOgQ7U4weJpG-u8sXxWC_ULD6NP3WFP6UUnYB6EMO4P71qBUl5aB9ma2aLsywqsPBMy8Rrn0w5v-fNUVR-qFI4sw3VjPvmUaoMyGI1X3WulxBsZ9ssHaFZIP1xh9PDIRt3W7mBJ4Mg_UZ-Sn31LU9gXMbcdGzELE4EGrICrNDoNihss0mAia2hJG9maHitmD13h0kPJwuJT-mY3jxL37bV7RSfcxGlNYOy7DQcQwAqdWxq6uZRyK2gyRYTomhiPcnzEzNMC_VKq2g2hQfkOXoi8IqKr1yOFEM-AnQHbNIhN7Gb0z3IZIvkQ1QCY1foIBdsDZ0NB6Q=w852-h637-no?authuser=0"
+            src="https://i.ibb.co/sPdgFXF/00000-IMG-00000-BURST20190628153201023-COVER.jpg"
           />
         </GridItem>
         <GridItem justifySelf="center" textAlign="center" margin="auto 30px">
